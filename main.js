55d21dabf3ed051cfaeba1ca10244b511b68d349
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2024-03-06 14:11:25 -0700

    Additions for 3/6 Metting
    Added values to options
    Added input slider
    Added else statement

--- main.js
+++ main.js
@@ -18,20 +18,40 @@ document.addEventListener('DOMContentLoaded', function(){
         // If the value is blue, it changes the background color to blue
         // If the value is green, it changes the background color to green
         // It will also display infomration about the color
-        if(option === "Red"){
+        if(option === "red"){
             body.style.backgroundColor = "red";
             information = "Red tends to show bravery"
-        } else if (option === "Blue"){
+        } else if (option === "blue"){
             body.style.backgroundColor = "blue";
             information = "Blue tends to show intelligence"
-        } else if (option === "Green"){
+        } else if (option === "green"){
             body.style.backgroundColor = "green";
             information = "Green tends to show kindness"
+        // This is an else, meaning if everything is false it runs
+        } else {
+            console.log("Not valid color selected")
+            console.log(option)
+            body.style.backgroundColor = "black";
+            information = "Please select a color"
         }
         // This gets the element with the id of text and saves it to the text variable
         var text = document.getElementById("text")
         // This changes the innerHTML of the text element to the information variable
         text.innerHTML = information
     });
+
+    // This gets the element with the id of slider and saves it to the slider variable
+    document.getElementById("slider").addEventListener("change", function() {
+        var slider = document.getElementById("slider").value
+        // This gets the element with the id of sliderValue and has the innerHTML be the slider value
+        document.getElementById("sliderValue").innerHTML = slider
+        // This changes the background color of the body element to the slider value
+        // There are multiple ways to type the value
+        // You can do string concatanation:
+        // body.style.backgroundColor = "rgb(0," + slider + ",0)"
+        // You can use template literals:
+        // body.style.backgroundColor = `rgb(0,${slider},0)`
+        document.getElementById("body").style.backgroundColor = `rgb(0,${slider},0)`
+    })
 });
 
